e18ce10181bcd667cec4dc93daa5f5f1b862befb
satyavarshitha <155371756+satyavarshitha@users.noreply.github.com>
2024-07-31 15:15:32 +0530

    import java.util.Scanner; public class SumTwoNumbers {     public static void main(String[] args) {         Scanner scanner = new Scanner(System.in);         int num1 = scanner.nextInt();         int num2 = scanner.nextInt();         int sum = num1 * num2;         System.out.println(sum);         scanner.close();   } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int sum = num1 * num2;
+        System.out.println(sum);
+        scanner.close();
+  }
+}
+
+
